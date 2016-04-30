a81112a2b2081402ceac14786104892ca0f666e4
Antoine Nguyen <tonio@ngyn.org>
2016-04-30 14:21:58 +0200

    Django 1.9 compat.

--- modoboa_dmarc/templatetags/dmarc_tags.py
+++ modoboa_dmarc/templatetags/dmarc_tags.py
@@ -5,6 +5,7 @@ import datetime
 from dateutil import relativedelta
 
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -15,7 +16,7 @@ def next_period(period):
     current = datetime.datetime.strptime("{}-1".format(period), "%Y-%W-%w")
     current += relativedelta.relativedelta(weeks=1)
     parts = current.isocalendar()
-    return "{}-{}".format(parts[0], parts[1] - 1)
+    return mark_safe("{}-{}".format(parts[0], parts[1] - 1))
 
 
 @register.simple_tag
@@ -24,4 +25,4 @@ def previous_period(period):
     current = datetime.datetime.strptime("{}-1".format(period), "%Y-%W-%w")
     current += relativedelta.relativedelta(weeks=-1)
     parts = current.isocalendar()
-    return "{}-{}".format(parts[0], parts[1] - 1)
+    return mark_safe("{}-{}".format(parts[0], parts[1] - 1))
