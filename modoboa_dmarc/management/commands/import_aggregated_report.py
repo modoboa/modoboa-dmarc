65c44a8a6a06ac2921c00e81db7fb4e3acfb2170
Antoine Nguyen <tonio@ngyn.org>
2017-02-14 16:36:30 +0100

    Django 1.10 support.

--- modoboa_dmarc/management/commands/import_aggregated_report.py
+++ modoboa_dmarc/management/commands/import_aggregated_report.py
@@ -1,30 +1,32 @@
 """Import a DMARC aggregated report."""
 
-from optparse import make_option
-
 from django.core.management.base import BaseCommand
 
 from ... import lib
 
 
 class Command(BaseCommand):
-
     """Command definition."""
 
     help = "Import DMARC aggregated reports."
 
-    option_list = BaseCommand.option_list + (
-        make_option("--pipe", action="store_true", default=False,
-                    help="Import a report from an email given on stdin"),
-        make_option("--imap", action="store_true", default=False,
-                    help="Import a report from an IMAP mailbox"),
-        make_option("--host", default="localhost",
-                    help="IMAP host"),
-        make_option("--ssl", action="store_true", default=False,
-                    help="Connect using SSL"),
-        make_option("--mailbox", default="INBOX",
-                    help="IMAP mailbox to import reports from"),
-    )
+    def add_arguments(self, parser):
+        """Add extra arguments to parser."""
+        parser.add_argument(
+            "--pipe", action="store_true", default=False,
+            help="Import a report from an email given on stdin"),
+        parser.add_argument(
+            "--imap", action="store_true", default=False,
+            help="Import a report from an IMAP mailbox"),
+        parser.add_argument(
+            "--host", default="localhost",
+            help="IMAP host"),
+        parser.add_argument(
+            "--ssl", action="store_true", default=False,
+            help="Connect using SSL"),
+        parser.add_argument(
+            "--mailbox", default="INBOX",
+            help="IMAP mailbox to import reports from"),
 
     def handle(self, *args, **options):
         """Entry point."""
