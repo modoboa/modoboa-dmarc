d9681333a7ea73576505a3a05aebd57f04d00867
Antoine Nguyen <tonio@ngyn.org>
2017-11-09 09:15:02 +0100

    Added unit test

--- modoboa_dmarc/tests/test_views.py
+++ modoboa_dmarc/tests/test_views.py
@@ -6,7 +6,6 @@ from modoboa.admin import factories as admin_factories
 from modoboa.core import models as core_models
 from modoboa.lib.tests import ModoTestCase
 
-from .. import models
 from . import mixins
 
 
@@ -18,6 +17,16 @@ class DMARCViewsTestCase(mixins.CallCommandMixin, ModoTestCase):
         """Create test data."""
         super(DMARCViewsTestCase, cls).setUpTestData()
         cls.domain = admin_factories.DomainFactory(name="ngyn.org")
+
+    def test_domainlist_view(self):
+        """Test domain list view."""
+        self.import_reports()
+        user = core_models.User.objects.get(username="admin")
+        self.client.force_login(user)
+        url = reverse("admin:_domain_list")
+        response = self.ajax_get(url)
+        url = reverse("modoboa_dmarc:domain_report", args=[self.domain.pk])
+        self.assertIn(url, response["rows"])
 
     def test_domainreport_view(self):
         """Test domain report view."""
