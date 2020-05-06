20b2f96c6980b363791f429a25f80c5d271772e2
Antoine Nguyen <tonio@ngyn.org>
2020-05-06 14:04:37 +0200

    Fix #40

--- setup.py
+++ setup.py
@@ -7,16 +7,11 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 """
 
-from __future__ import unicode_literals
-
 import io
 from os import path
 from setuptools import setup, find_packages
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 
 
 def get_requirements(requirements_file):
@@ -24,12 +19,7 @@ def get_requirements(requirements_file):
     requirements = []
     if path.isfile(requirements_file):
         for req in parse_requirements(requirements_file, session="hack"):
-            # check markers, such as
-            #
-            #     rope_py3k    ; python_version >= '3.0'
-            #
-            if req.match_markers():
-                requirements.append(str(req.req))
+            requirements.append(req.requirement)
     return requirements
 
 
